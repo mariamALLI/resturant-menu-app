f604d738181c3c21503e32ab44193e761618e7b8
Mariam O-Alli <81536650+mariamALLI@users.noreply.github.com>
2023-01-20 10:43:10 +0100

    initialize commit

---
+++ data.js
@@ -0,0 +1,26 @@
+export const menuArray = [
+    {
+        name: "Pizza",
+        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        id: 0,
+        price: 14,
+        emoji: "🍕",
+        photo: "images/pizza.png"
+    },
+    {
+        name: "Hamburger",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        emoji: "🍔",
+        id: 1,
+        photo: "images/burger.png"
+    },
+        {
+        name: "Beer",
+        ingredients: ["grain, hops, yeast, water"],
+        price: 12,
+        emoji: "🍺",
+        id: 2,
+        photo: "images/drink.png"
+    }
+]
