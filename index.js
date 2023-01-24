53ed47009cbaeb55d285b0de7974ddbe891c2aa6
Mariam Oluseyi-Alli <81536650+mariamALLI@users.noreply.github.com>
2023-01-24 19:46:01 +0100

    second commit
    displaying the menu feed using js

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 import { menuArray } from "./data.js";
+
+// const menuList = document.getElementById("menu-list")
+const menuBtn = document.querySelector('#menu-btn')
+
+function getMenuHtml(){
+    let menuHtml = '';
+
+    menuArray.forEach((menu)=>{
+       
+        menuHtml += `
+        <div class="menu-container">
+            <img src=${menu.photo} alt="pizza slice 🍕 ">
+
+            <div class="menu-text">
+                <h2 class="menu-title">${menu.name}</h2>
+                <p class="menu-receipe">${menu.ingredients}</p>
+                <p class="menu-price">${menu.price}</p>
+            </div>
+            <button class="menu-btn" id="menu-btn">+</button>
+        </div>
+
+        <div class="line"></div>
+        `
+    })
+    return menuHtml
+}
+
+function renderMenuList(){
+    const menuList = document.getElementById("menu-list")
+    menuList.innerHTML = getMenuHtml()
+}
+// call renderMenu
+renderMenuList()
