2e97ef7b7ccd12d1a52aa97c9bb44dc557ad0dc7
Mariam Oluseyi-Alli <81536650+mariamALLI@users.noreply.github.com>
2023-01-29 15:41:31 +0100

    menu button
    menu btn working due to dataset attribute

--- index.js
+++ index.js
@@ -1,34 +1,92 @@
 import { menuArray } from "./data.js";
 
-// const menuList = document.getElementById("menu-list")
-const menuBtn = document.querySelector('#menu-btn')
+function handleAllBtns(e){
+    if(e.target.dataset.menu){
+        getAddedOrders(e.target.dataset.menu)
+    }
+}
+
 
 function getMenuHtml(){
     let menuHtml = '';
-
+    
     menuArray.forEach((menu)=>{
-       
+    
         menuHtml += `
+       
         <div class="menu-container">
-            <img src=${menu.photo} alt="pizza slice 🍕 ">
-
-            <div class="menu-text">
-                <h2 class="menu-title">${menu.name}</h2>
-                <p class="menu-receipe">${menu.ingredients}</p>
-                <p class="menu-price">${menu.price}</p>
-            </div>
-            <button class="menu-btn" id="menu-btn">+</button>
+          <figure>${menu.emoji}</figure>
+            
+                <div class="menu-text">
+                    <h2 class="menu-title">${menu.name}</h2>
+                    <p class="menu-receipe">${menu.ingredients}</p>
+                    <p class="menu-price">$${menu.price}</p>
+                </div>
+                <i class="fa-solid fa-circle-plus menu-btn" id="menu-btn" data-menu="${menu.name}"></i>
+        
         </div>
 
         <div class="line"></div>
         `
     })
-    return menuHtml
+    document.getElementById("menu-list").innerHTML = menuHtml;
+    document.addEventListener('click', (e)=> handleAllBtns(e))
+}
+ getMenuHtml()
+
+
+
+let selectedOrderMenu = []
+
+function getAddedOrders(selectedOrder){
+    menuArray.forEach((order)=>{
+        if(order.name === selectedOrder){
+            selectedOrderMenu.push(order)
+            console.log('added order : ' ,selectedOrderMenu)
+            console.log(selectedOrderMenu.indexOf(order))
+            orderListMenu()
+        }
+    })
+
 }
 
-function renderMenuList(){
-    const menuList = document.getElementById("menu-list")
-    menuList.innerHTML = getMenuHtml()
+function orderListMenu(){
+    let orderMenu = '';
+    let ordered = '';
+    let totalOrder = '';
+    let price = 0
+
+    if(selectedOrderMenu.length > 0){
+        selectedOrderMenu.forEach((order)=>{
+            orderMenu = '<h2 class="title"> Yourorder</h2>'
+
+            ordered += `
+                    <div class=order-request>
+                       <h2 class="order-title">${order.name}</h2>
+                        <button class="remove-btn" id="remove-btn" type="submit" data-remove="${order.name}">remove</button> 
+                        <h3 class="order-price">$${order.price}</h3>
+                    </div>
+            ` 
+
+            price += order.price
+
+            totalOrder = `
+                <div class="total">
+                    <h2 class="total-title">Total price:</h2>
+                    <p class="ordered-price">$${price}</p>
+                </div>
+
+                <button class="btn" id="complete-order-btn" type="submit">Complete order</button>
+            `
+        })
+    }
+
+    document.getElementById('order-menu').innerHTML = orderMenu;
+    document.getElementById('ordered').innerHTML = ordered;
+    document.getElementById('total-order').innerHTML = totalOrder;
 }
-// call renderMenu
-renderMenuList()
+
+
+
+
+
